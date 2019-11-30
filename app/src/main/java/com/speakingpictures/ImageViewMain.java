b7cef7a1468c2e1e582767c3a4e22fe82a84d91a
Tsvetan Trifonov <tsmtr_vd@abv.bg>
2019-11-30 18:19:25 +0200

    GOOD minor comment old img loading

--- app/src/main/java/com/speakingpictures/ImageViewMain.java
+++ app/src/main/java/com/speakingpictures/ImageViewMain.java
@@ -198,7 +198,7 @@ public class ImageViewMain extends Activity implements
             currentPicName = file.getName();
             Picasso.with(getApplicationContext()).load(file).fit().centerInside().into(imageView);
 
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));//not needed
             setRectanglesInsideCustomView(imageView);
 
 
